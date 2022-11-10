8f72c3ada2dd29ef97f47c851111777fbfb18c43
avocad06 <miggo2704@gmail.com>
2022-11-10 16:55:51 +0900

    style: 회원정보 페이지 스타일 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 # Create your models here.
 class User(AbstractUser):
+    profile = ProcessedImageField(
+        blank=True,
+        processors=[Thumbnail(120, 120)],
+        format="JPEG",
+        options={"quality": 90},
+    )
     nickname = models.CharField(max_length=15, unique=True, null=True)
     
     def __str__(self):
