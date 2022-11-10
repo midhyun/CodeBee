19587dc74e3d1cf289c859037724d36931d0d3fd
midhyun <kfree912@gmail.com>
2022-11-10 15:53:48 +0900

    feat/base.html

--- pjt/settings.py
+++ pjt/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'pjt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- pjt/urls.py
+++ pjt/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reviews import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home, name='home')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
