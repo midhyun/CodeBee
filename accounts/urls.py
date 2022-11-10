da929fbc10ab07839a73c41f717cb0f6093f2b45
mhd329 <mhd329@gmail.com>
2022-11-11 05:56:12 +0900

    accounts/socialLogin 깃 관련해서는 로그인 문제가 있을 수 있습니다. 문제가 발생할 경우 연관된 코드들을 주석처리하고 진행해주세요.
    수정 내역 : accounts/ 소셜로그인 로직 함수화하여 정리, 유저 모델 다소 수정, urls.py 소셜로그인 부분 정규표현식으로 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "accounts"
@@ -9,15 +9,9 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
-    ### 소셜로그인 ###
-    path("login/kakao/", views.kakao_request, name="kakao"),
-    path("login/kakao/callback/", views.kakao_callback),
-    path("login/naver/", views.naver_request, name="naver"),
-    path("login/naver/callback/", views.naver_callback),
-    path("login/google/", views.google_request, name="google"),
-    path("login/google/callback/", views.google_callback),
-    path("login/github/", views.github_request, name="github"),
-    path("login/github/callback/", views.github_callback),
-    ### 소셜로그인 ###
+    re_path(
+        r"^login/(?P<service_name>[^/]+)/$", views.social_login_request, name="social"
+    ),
+    re_path(r"^login/(?P<service_name>[^/]+)/callback/$", views.social_login_callback),
     path("test/", views.test, name="test"),
 ]
