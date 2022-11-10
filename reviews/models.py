483a7b99d757e73b9f899a9c79338262491fcde9
runedemonic <runedemonic@naver.com>
2022-11-10 18:07:45 +0900

    review test

--- reviews/models.py
+++ reviews/models.py
@@ -1,35 +1,35 @@
 from django.db import models
+from django.conf import settings
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
-
 # Create your models here.
+class Study(models.Model):
+    limits = models.IntegerField(default=4)
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    tag = models.CharField(max_length=20, blank=True)
+    categorie = models.CharField(max_length=30)
+    study_type = models.CharField(max_length=30)
+    deadline = models.DateField()
+    location_type = models.BooleanField(default=False) # False - 오프라인
+    location = models.CharField(max_length=50, blank=True)
+    X = models.CharField(max_length=20, null=True)
+    Y = models.CharField(max_length=20, null=True)
+    image = ProcessedImageField(
+        upload_to="images/",
+        blank=True,
+        format="JPEG",
+    )
+    joined_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="joined")
 
+class Accepted(models.Model):
+    joined = models.BooleanField(default=False)
+    study = models.ForeignKey(Study, on_delete=models.CASCADE)
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
 
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     date = models.DateField()
-
-
-class Article(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models)
-    title = models.CharField(max_length=80)
-    content = models.TextField()
-    category = models.CharField(max_length=80)
-    image = ProcessedImageField(
-        upload_to='articles',
-        processors=[ResizeToFill(100, 100)],
-        format='JPEG',
-        options={
-            'quality': 90,
-        }
-
-    )
-    study_type = models.CharField(max_length=80)
-    location = models.CharField(max_length=100)
-    tags = models.CharField(max_length=50)
-    x = models.IntegerField(default= 0)
-    y = models.IntegerField(default= 0)
-    limit = models.IntegerField(default=1)
-    deadline = models.DateField(auto_now_add=True,blank=True)
