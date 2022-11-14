16f67ea311582cddaf98b68d8897110b7d1dcee3
midhyun <kfree912@gmail.com>
2022-11-14 17:24:25 +0900

    gathering_btn

--- reviews/views.py
+++ reviews/views.py
@@ -6,6 +6,8 @@ import requests
 import json
 from django.contrib import messages
 # Create your views here.
+# 카카오톡 나에게 보내기 메시지 url
+url="https://kapi.kakao.com/v2/api/talk/memo/default/send"
 
 def home(request):
     return render(request, 'home.html')
@@ -95,7 +97,7 @@ def join(request, study_pk, user_pk):
     if study.limits > len(accepted):
         for joined in users:
             if joined in accepted:
-                messages.warning(request, '이미 가입되어 있습니다.')
+                messages.warning(request, '이미 가입신청 한 그룹입니다.')
                 return redirect('reviews:detail', study_pk)
         else:
             Aform = Accepted(joined=False,study=study,users=request.user)
@@ -105,14 +107,15 @@ def join(request, study_pk, user_pk):
             data = {"template_object": json.dumps({
             "object_type": "feed",
             "content": {
-                "title": f"{request.user}님의 스터디 가입신청!",
+                "title": f"{request.user}님의 스터디 가입신청! ({len(accepted)} / {study.limits})",
                 "description": "신청을 승인해주세요!",
-                "image_url": f"http://localhost:8000{image_url}",
+                "image_url": f"https://user-images.githubusercontent.com/108651809/201609398-060cbab1-1ff4-440f-a989-9ab77965eb94.png",
+                # "image_url": f"http://localhost:8000{image_url}",
                 "image_width": 800,
                 "image_height": 563,
                 "link": {
-                    "web_url": "http://localhost:8000/",
-                    "mobile_web_url": "http://localhost:8000/",
+                    "web_url": "http://localhost:8000",
+                    "mobile_web_url": "http://localhost:8000",
                     "android_execution_params": "contentId=100",
                     "ios_execution_params": "contentId=100"
                 }
@@ -121,8 +124,8 @@ def join(request, study_pk, user_pk):
                 {
                     "title": "웹으로 이동",
                     "link": {
-                        "web_url": "http://www.daum.net",
-                        "mobile_web_url": "http://m.daum.net"
+                        "web_url": "http://localhost:8000",
+                        "mobile_web_url": "http://localhost:8000"
                     }
                 },
                 {
@@ -140,6 +143,7 @@ def join(request, study_pk, user_pk):
             messages.success(request, '가입 신청이 완료되었습니다. 호스트의 승인을 기다려 주세요.')
             return redirect('reviews:detail', study_pk)
     else:
+        messages.success(request, '모집인원이 가득 찬 그룹입니다.')
         return redirect('reviews:detail', study_pk)
 
 def study_accepted(request, study_id, users_id):
@@ -165,28 +169,56 @@ def study_kick(request, study_id, users_id):
         return redirect('reviews:index')
     else:
         return redirect('reviews:userlist', study_id)
-url="https://kapi.kakao.com/v2/api/talk/memo/default/send"
 
 
 def gathering(request, study_pk):
     accepted = Accepted.objects.filter(study_id=study_pk, joined=1)
     study = Study.objects.get(pk=study_pk)
     message = request.POST["message"]
-    data={
-    "template_object": json.dumps({
-        "object_type":"text",
-        "text": f"{study.host.username}으로부터의 메시지 \n {message}",
-        "link":{
-            "web_url":"www.naver.com",
-            "mobile_url":"www.naver.com"
-        }})}
+    try: image_url = study.image.url
+    except: image_url = ''
+    data = {"template_object": json.dumps({
+            "object_type": "feed",
+            "content": {
+                "title": f"{request.user}님의 메시지",
+                "description": f"{message}",
+                "image_url": f"https://user-images.githubusercontent.com/108651809/201609398-060cbab1-1ff4-440f-a989-9ab77965eb94.png",
+                # "image_url": f"http://localhost:8000{image_url}",
+                "image_width": 800,
+                "image_height": 550,
+                "link": {
+                    "web_url": "http://localhost:8000",
+                    "mobile_web_url": "http://localhost:8000",
+                    "android_execution_params": "contentId=100",
+                    "ios_execution_params": "contentId=100"
+                }
+            },
+            "buttons": [
+                {
+                    "title": "웹으로 이동",
+                    "link": {
+                        "web_url": "http://localhost:8000",
+                        "mobile_web_url": "http://localhost:8000"
+                    }
+                },
+                {
+                    "title": "앱으로 이동",
+                    "link": {
+                        "android_execution_params": "contentId=100",
+                        "ios_execution_params": "contentId=100"
+                    }
+                }
+            ]
+            })}
     for user in accepted:
         token = user.users.token
         if token:
             headers={"Authorization" : "Bearer " + token}
             response = requests.post(url, headers=headers, data=data)
             print(str(response.json()))
+            messages.success(request, '메시지 전송이 완료되었습니다.')
         else:
+            print('no')
             pass
     
     return redirect('reviews:detail', study_pk)
