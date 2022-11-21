f777263c10715712667148bd0919089ed6f37457
runedemonic <runedemonic@naver.com>
2022-11-21 23:54:34 +0900

    deploy

--- pjt/settings.py
+++ pjt/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 ALLOWED_HOSTS = [
     "codebee-env-1.eba-ybm4hjsv.ap-northeast-2.elasticbeanstalk.com",
+    'www.runedemon.shop',
     "172.31.13.105",
     "172.31.10.233",
     "127.0.0.1",
