d15290ed18d00184edb37aefa8260c565413cce1
midhyun <kfree912@gmail.com>
2022-11-23 18:04:04 +0900

    readme

--- pjt/settings.py
+++ pjt/settings.py
@@ -100,26 +100,26 @@ WSGI_APPLICATION = "pjt.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-#
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-#
-# else:
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DATABASE_NAME"),  # .env 파일에 value 작성
-        "USER": "postgres",
-        "PASSWORD": os.getenv("DATABASE_PASSWORD"),  # .env 파일에 value 작성
-        "HOST": os.getenv("DATABASE_HOST"),  # .env 파일에 value 작성
-        "PORT": "5432",
+DEBUG=True
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DATABASE_NAME"),  # .env 파일에 value 작성
+            "USER": "postgres",
+            "PASSWORD": os.getenv("DATABASE_PASSWORD"),  # .env 파일에 value 작성
+            "HOST": os.getenv("DATABASE_HOST"),  # .env 파일에 value 작성
+            "PORT": "5432",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
